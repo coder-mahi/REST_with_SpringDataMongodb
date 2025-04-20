adb4dea8c3f302d66c20b6410793fe0e46efaeed
MAHESH <maheshshinde9100@gmail.com>
2025-04-20 14:38:14 +0530

    Add Lombok dependency and apply @Getter and @Setter annotations to User and JournalEntry entities

--- DemoApp/src/main/java/com/mahesh/entity/User.java
+++ DemoApp/src/main/java/com/mahesh/entity/User.java
@@ -3,31 +3,38 @@ package com.mahesh.entity;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Document(collection="user_entries")
+@Getter
+@Setter
+// @Data  --> combination of @Getter and @Setter
 public class User {
 	@Id
 	private ObjectId id;
 	private String name;
 	private String email;
-	public ObjectId getId() {
-		return id;
-	}
-	public void setId(ObjectId id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
+	
+//	public ObjectId getId() {
+//		return id;
+//	}
+//	public void setId(ObjectId id) {
+//		this.id = id;
+//	}
+//	public String getName() {
+//		return name;
+//	}
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//	public String getEmail() {
+//		return email;
+//	}
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
 	
 	
 }
